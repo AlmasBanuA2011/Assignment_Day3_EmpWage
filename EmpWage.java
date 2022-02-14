7938512721833703e58daf53de8c1d9e706b8631
Almas Banu A <99530020+AlmasBanuA2011@users.noreply.github.com>
2022-02-14 22:07:24 +0530

    EmpWage UC1 Added

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,11 @@
 public class EmpWage {
+    public static void main(String[] args) {
+        int IS_FULL_TIME = 1;
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to EmpWage Computation");
-	}
-
+        double empcheck= Math.floor(Math.random() *10) % 2;
+        if (empcheck == IS_FULL_TIME)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is Absent");
+    }
 }
