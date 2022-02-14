6615bb95e090546dbfd550c3c495a7e78c59856d
Almas Banu A <99530020+AlmasBanuA2011@users.noreply.github.com>
2022-02-14 22:10:08 +0530

    EmpWage UC2 Added

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,13 @@
 public class EmpWage {
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to EmpWage Computation");
-	}
-
+    public static void main(String[] args) {
+      int IS_FULL_TIME = 1;
+      int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+	int totalWage = 0;
+        double empcheck= Math.floor(Math.random() *10) % 2;
+        if (empcheck == IS_FULL_TIME)
+            empHrs = 8;
+        totalWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp WAge: " + totalWage);
+    }
 }
